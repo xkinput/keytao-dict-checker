7be9261819a90ee7e301207eb285795b487944ec
Garth TB | 天卜 <g-art-h@outlook.com>
2026-05-28 20:08:55 +0800

    将文件头的检验逻辑提取为关联函数

--- src/dict.rs
+++ src/dict.rs
@@ -11,30 +11,7 @@ impl Dict {
     pub(crate) fn load(path: &str) -> crate::DynResult<Self> {
         let s = std::fs::read_to_string(path)?;
         let mut lines = s.lines().enumerate();
-
-        // 丢弃文件头前的内容
-        while lines.next().is_some_and(|(_, l)| l != "---") {}
-
-        let mut yaml = String::with_capacity(1024);
-        loop {
-            match lines.next() {
-                None => return Err("词库文件头缺失或未闭合".into()),
-                Some((_, "...")) => break,
-                Some((_, l)) if yaml.len() < 65536 => yaml.push_str(l),
-                _ => return Err("词库文件头过长，疑似未闭合".into()),
-            }
-            yaml.push('\n');
-        }
-
-        #[derive(serde::Deserialize)]
-        struct Header {
-            columns: Option<Vec<String>>,
-        }
-        let expected = ["text", "code", "weight"];
-        let header: Header = yaml_serde::from_str(&yaml)?;
-        if header.columns.is_some_and(|cols| cols != expected) {
-            return Err(format!("词库列配置无效：应为{expected:?}").into());
-        }
+        Self::verify_and_rem_header(&mut lines)?;
 
         let mut entries = Vec::with_capacity(65536);
         for (i, l) in lines {
@@ -61,6 +38,37 @@ impl Dict {
             texts,
             codes,
         })
+    }
+
+    pub(crate) fn verify_and_rem_header(
+        lines: &mut std::iter::Enumerate<core::str::Lines>,
+    ) -> crate::DynResult<()> {
+        // 丢弃文件头前的内容
+        while lines.next().is_some_and(|(_, l)| l != "---") {}
+
+        let mut yaml = String::with_capacity(1024);
+        loop {
+            match lines.next() {
+                None => return Err("词库文件头缺失或未闭合".into()),
+                Some((_, "...")) => break,
+                Some((_, l)) if yaml.len() < 65536 => yaml.push_str(l),
+                _ => return Err("词库文件头过长，疑似未闭合".into()),
+            }
+            yaml.push('\n');
+        }
+
+        #[derive(serde::Deserialize)]
+        struct Header {
+            columns: Option<Vec<String>>,
+        }
+        let expected = ["text", "code", "weight"];
+        let header: Header = yaml_serde::from_str(&yaml)?;
+        match header.columns {
+            Some(cols) if cols != expected => {
+                Err(format!("词库列配置无效：应为{expected:?}，实为{cols:?}").into())
+            }
+            _ => Ok(()),
+        }
     }
 
     pub(crate) fn for_each_vacant_code(&self, f: impl FnMut(&str)) {
