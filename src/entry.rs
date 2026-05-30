9f5f24129113b358de55a542b159160ee6200760
Garth TB | 天卜 <g-art-h@outlook.com>
2026-05-30 16:50:04 +0800

    优化词条模型

--- src/entry.rs
+++ src/entry.rs
@@ -1,47 +1,64 @@
-use std::rc::Rc;
+use std::{fmt::Display, rc::Rc};
 
-pub(crate) struct Entry {
-    text: Rc<str>,
-    code: String,
-    weight: Option<String>,
+pub(crate) struct Entry<T> {
+    line_num: usize,
+    pub(crate) text: T,
+    pub(crate) code: String,
+    raw: String,
 }
 
-impl Entry {
-    pub(crate) fn new(num: usize, line: &str) -> crate::DynResult<Self> {
-        let mut parts = line.splitn(4, '\t');
-        let text = match parts.next().unwrap().trim() {
-            "" => return Err(format!("第{num}行词条缺失文本").into()),
-            trimmed => trimmed.into(),
-        };
-        let code = match parts.next().map(|s| s.trim()) {
-            Some(s) if !s.is_empty() => s.into(),
-            _ => return Err(format!("第{num}行词条缺失编码").into()),
-        };
-        let weight = parts
-            .next()
-            .map(|s| s.trim())
-            .filter(|s| !s.is_empty())
-            .map(|s| s.into());
-        if parts.next().is_some() {
-            return Err(format!("第{num}行词条有多余内容").into());
+impl<T: Display> Entry<T> {
+    pub(crate) fn to_str(&self) -> String {
+        format!("第{}行：'{}'", self.line_num, self.raw)
+    }
+}
+
+pub(crate) trait TextParser: Sized {
+    fn parse(num: usize, trimmed_text: &str) -> crate::DynResult<Self>;
+}
+
+impl<T: TextParser> Entry<T> {
+    pub(crate) fn new(line_num: usize, line: &str) -> crate::DynResult<Option<Self>> {
+        let l = line.trim();
+        if l.is_empty() || l.starts_with('#') {
+            return Ok(None);
         }
-
-        Ok(Self { text, code, weight })
+        let mut parts = l.splitn(4, '\t');
+        Ok(Some(Self {
+            line_num,
+            text: match parts.next().unwrap().trim() {
+                "" => return Err(format!("第{line_num}行词条缺失文本").into()),
+                trimmed => T::parse(line_num, trimmed)?,
+            },
+            code: match parts.next().map(|s| s.trim()) {
+                Some("") | None => return Err(format!("第{line_num}行词条缺失编码").into()),
+                Some(s) => s.into(),
+            },
+            raw: l.into(),
+        }))
     }
+}
 
-    pub(crate) fn text(&self) -> Rc<str> {
-        self.text.clone()
-    }
+pub(crate) type Single = Entry<char>;
 
-    pub(crate) fn single(&self) -> Option<char> {
-        let mut chars = self.text.chars();
-        match (chars.next(), chars.next()) {
-            (Some(single), None) => Some(single),
-            _ => None,
+impl TextParser for char {
+    fn parse(line_num: usize, trimmed_text: &str) -> crate::DynResult<Self> {
+        let mut chars = trimmed_text.chars();
+        let c = chars.next().unwrap(); // 一定非空
+        if chars.next().is_some() {
+            return Err(format!("第{line_num}行词条的文本不是单字").into());
         }
+        Ok(c)
     }
+}
 
-    pub(crate) fn code(&self) -> &str {
-        &self.code
+pub(crate) type Phrase = Entry<Rc<str>>;
+
+impl TextParser for Rc<str> {
+    fn parse(line_num: usize, trimmed_text: &str) -> crate::DynResult<Self> {
+        if trimmed_text.chars().count() < 2 {
+            return Err(format!("第{line_num}行词条的文本不是词组").into());
+        }
+        Ok(trimmed_text.into())
     }
 }
