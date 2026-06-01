806c388cb23a2238282adf5656baec67f6a0eb80
Garth TB | 天卜 <g-art-h@outlook.com>
2026-06-02 07:18:36 +0800

    优化空码检查逻辑

--- src/trie.rs
+++ src/trie.rs
@@ -1,15 +1,15 @@
 pub(crate) struct Trie<T> {
     children: Vec<rustc_hash::FxHashMap<char, usize>>,
-    values: Vec<Vec<T>>,
+    vals: Vec<Vec<T>>,
 }
 
 impl<T> Trie<T> {
     pub(crate) fn with_capacity(n: usize) -> Self {
         let mut children = Vec::with_capacity(n + 1);
         children.push(Default::default());
-        let mut values = Vec::with_capacity(n + 1);
-        values.push(vec![]);
-        Self { children, values }
+        let mut vals = Vec::with_capacity(n + 1);
+        vals.push(vec![]);
+        Self { children, vals }
     }
 
     pub(crate) fn insert(&mut self, k: &str, v: T) {
@@ -19,32 +19,31 @@ impl<T> Trie<T> {
             i = *self.children[i].entry(c).or_insert(j);
             if i == j {
                 self.children.push(Default::default());
-                self.values.push(vec![]);
+                self.vals.push(vec![]);
             }
         }
-        self.values[i].push(v);
+        self.vals[i].push(v);
     }
 
-    pub(crate) fn for_each_vacant(&self, mut f: impl FnMut(&str)) {
-        self.dfs(0, 0, &mut String::new(), &mut f);
+    pub(crate) fn vacant_codes(&self) -> Vec<String> {
+        let mut codes = Vec::new();
+        self.dfs(0, &mut String::new(), &mut codes);
+        codes
     }
 
-    fn dfs(&self, i: usize, len: usize, code: &mut String, f: &mut impl FnMut(&str)) -> u8 {
-        let mut v = if self.values[i].is_empty() { 0 } else { 1 };
+    fn dfs(&self, i: usize, code: &mut String, codes: &mut Vec<String>) -> bool {
+        let mut has_val = !self.vals[i].is_empty();
         for (&c, &j) in &self.children[i] {
             code.push(c);
-            let cv = self.dfs(j, len + 1, code, f);
+            let child_has_val = self.dfs(j, code, codes);
             code.pop();
-            if cv > 0 {
-                v |= cv;
-                if len >= 3 && !matches!(c, 'a' | 'i' | 'o' | 'u' | 'v') {
-                    v |= 2;
-                }
+            if matches!(c, 'a' | 'i' | 'o' | 'u' | 'v') && child_has_val {
+                has_val = true;
             }
         }
-        if self.values[i].is_empty() && (len >= 4 && v != 0 || len == 3 && v == 1) {
-            f(code);
+        if code.len() >= 3 && self.vals[i].is_empty() && has_val {
+            codes.push(code.clone());
         }
-        v
+        has_val
     }
 }
