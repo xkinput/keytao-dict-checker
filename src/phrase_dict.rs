54d419a197ca3826336ef172dc6c4fdff60280fc
Garth TB | 天卜 <g-art-h@outlook.com>
2026-06-02 07:24:28 +0800

    优化词库模型

--- src/phrase_dict.rs
+++ src/phrase_dict.rs
@@ -1,44 +1,46 @@
-use crate::{DynResult, dict::DictLoader, entry::Phrase, trie::Trie};
+use crate::{DynResult, dict::DictLoader, entry::Phrase, single_dict::SingleDict, trie::Trie};
 use rustc_hash::FxHashMap;
 
 type TextMap = FxHashMap<std::rc::Rc<str>, Vec<usize>>;
 
 pub(crate) struct PhraseDict {
-    entries: Vec<Phrase>,
+    pool: Vec<Phrase>,
     texts: TextMap,
     codes: Trie<usize>,
 }
 
 impl DictLoader for PhraseDict {
     fn parse_lines<'a>(lines: &mut impl Iterator<Item = (usize, &'a str)>) -> DynResult<Self> {
-        let mut entries = Vec::with_capacity(65536);
+        let mut pool = Vec::with_capacity(65536);
         for (i, l) in lines {
             if let Some(phrase) = Phrase::new(i + 1, l)? {
-                entries.push(phrase);
+                pool.push(phrase);
             }
         }
-        let cnt = entries.len();
+        let cnt = pool.len();
         if cnt == 0 {
             return Err("词库为空".into());
         }
 
         let mut texts: TextMap = FxHashMap::with_capacity_and_hasher(cnt, Default::default());
         let mut codes = Trie::with_capacity(4 * cnt);
-        for (i, e) in entries.iter().enumerate() {
+        for (i, e) in pool.iter().enumerate() {
             texts.entry(e.text.clone()).or_default().push(i);
             codes.insert(&e.code, i);
         }
 
-        Ok(Self {
-            entries,
-            texts,
-            codes,
-        })
+        Ok(Self { pool, texts, codes })
     }
 }
 
 impl PhraseDict {
-    pub(crate) fn for_each_vacant_code(&self, f: impl FnMut(&str)) {
-        self.codes.for_each_vacant(f)
+    pub(crate) fn vacant_codes(&self) -> Vec<String> {
+        self.codes.vacant_codes()
+    }
+
+    pub(crate) fn invalid_encodings(&self, single: &SingleDict) -> impl Iterator<Item = &Phrase> {
+        self.pool
+            .iter()
+            .filter(|phrase| !single.is_valid_encoding(phrase))
     }
 }
