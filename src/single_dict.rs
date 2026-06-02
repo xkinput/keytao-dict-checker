2037017b374b2bda0505ad66e88716471894fda0
Garth TB | 天卜 <g-art-h@outlook.com>
2026-06-03 02:34:31 +0800

    优化单字码表并实现编码检查逻辑

--- src/single_dict.rs
+++ src/single_dict.rs
@@ -1,25 +1,25 @@
 use crate::{DynResult, dict::DictLoader, entry};
 use rustc_hash::FxHashMap;
 
-type StemMap = FxHashMap<char, Vec<String>>;
-
 pub(crate) struct SingleDict {
-    stems: StemMap,
+    stems: FxHashMap<char, Vec<[char; 3]>>,
 }
 
 impl DictLoader for SingleDict {
     fn parse_lines<'a>(lines: &mut impl Iterator<Item = (usize, &'a str)>) -> DynResult<Self> {
-        let mut stems: StemMap = FxHashMap::with_capacity_and_hasher(8192, Default::default());
+        let mut stems = FxHashMap::with_capacity_and_hasher(8192, Default::default());
         for (i, l) in lines {
             if let Some(single) = entry::Single::new(i + 1, l)? {
-                let code = single.code;
-                let mut ci = code.char_indices();
-                if ci.nth(2).is_none() {
-                    continue;
+                let mut chars = single.code.chars();
+                if let Some(a) = chars.next()
+                    && let Some(b) = chars.next()
+                    && let Some(c) = chars.next()
+                {
+                    stems
+                        .entry(single.text)
+                        .or_insert_with(Vec::new)
+                        .push([a, b, c]);
                 }
-                let end = ci.next().map_or(code.len(), |(i, _)| i);
-                let stem = code[..end].to_string();
-                stems.entry(single.text).or_default().push(stem);
             }
         }
         if stems.is_empty() {
@@ -37,14 +37,61 @@ impl DictLoader for SingleDict {
 
 impl SingleDict {
     pub(crate) fn is_err_encoding(&self, phrase: &entry::Phrase) -> bool {
-        let mut text: Vec<_> = phrase.text.chars().collect();
-        if text.len() < 2 {
-            return false;
+        let mut text_chars = phrase.text.chars();
+        let mut valid_text: Vec<_> = (&mut text_chars).take(4).collect();
+        if valid_text.len() < 2 {
+            return true;
         }
-        if text.len() > 4 {
-            text[3] = text.pop().unwrap();
-            text.truncate(4);
+        if let Some(c) = text_chars.next_back() {
+            valid_text[3] = c;
         }
-        todo!()
+
+        let code: Vec<_> = phrase.code.chars().collect();
+        if code.len() < 3 || (valid_text.len() != 3 && code.len() < 4) || 6 < code.len() {
+            return true;
+        }
+
+        let Some(codes0) = self.stems.get(&valid_text[0]) else {
+            return true;
+        };
+        let Some(codes1) = self.stems.get(&valid_text[1]) else {
+            return true;
+        };
+        match valid_text.get(2..) {
+            Some([]) => !codes0.iter().any(|a| {
+                codes1.iter().any(|b| {
+                    code.starts_with(&[a[0], a[1], b[0], b[1]])
+                        && code.get(4).is_none_or(|&x| x == a[2])
+                        && code.get(5).is_none_or(|&x| x == b[2])
+                })
+            }),
+            Some([c2]) if let Some(codes2) = self.stems.get(c2) => !codes0.iter().any(|a| {
+                codes1.iter().any(|b| {
+                    codes2.iter().any(|c| {
+                        code.starts_with(&[a[0], b[0], c[0]])
+                            && code.get(3).is_none_or(|&x| x == a[2])
+                            && code.get(4).is_none_or(|&x| x == b[2])
+                            && code.get(5).is_none_or(|&x| x == c[2])
+                    })
+                })
+            }),
+            Some([c2, c3])
+                if let Some(codes2) = self.stems.get(c2)
+                    && let Some(codes3) = self.stems.get(c3) =>
+            {
+                !codes0.iter().any(|a| {
+                    codes1.iter().any(|b| {
+                        codes2.iter().any(|c| {
+                            codes3.iter().any(|d| {
+                                code.starts_with(&[a[0], b[0], c[0], d[0]])
+                                    && code.get(4).is_none_or(|&x| x == a[2])
+                                    && code.get(5).is_none_or(|&x| x == b[2])
+                            })
+                        })
+                    })
+                })
+            }
+            _ => true,
+        }
     }
 }
