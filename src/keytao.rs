b34fd0fa4e4cba7a116f3cf08a9fc0bf4c5ac196
Garth TB | 天卜 <g-art-h@outlook.com>
2026-09-07 15:40:46 +0800

    优化空码检查模块

--- src/keytao.rs
+++ src/keytao.rs
@@ -1,21 +1,29 @@
+/// 编码最大长度
+pub(crate) const MAX_CODE_LEN: usize = 6;
+
 /// 是否为形码
 fn is_xm(b: &&u8) -> bool {
     matches!(b, b'a' | b'i' | b'o' | b'u' | b'v')
 }
 
 /// 编码的形码长度
-pub(crate) fn xm_len(code: &str) -> usize {
+fn xm_len(code: &str) -> usize {
     code.as_bytes().iter().rev().take_while(is_xm).count()
 }
 
 /// 编码的音码长度
-pub(crate) fn ym_len(code: &str) -> usize {
+fn ym_len(code: &str) -> usize {
     code.len() - xm_len(code)
 }
 
 /// 编码的音码段
 pub(crate) fn ym(code: &str) -> &[u8] {
     &code.as_bytes()[..ym_len(code)]
+}
+
+/// 所有更短合法编码
+pub(crate) fn shorter(code: &str) -> impl Iterator<Item = &str> {
+    (ym_len(code)..code.len()).map(|len| &code[..len])
 }
 
 /// 单字编码的构词码
